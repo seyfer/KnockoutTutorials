052a246fafee9339541e466e83711c776f9f0f5b
seyfer <seyferseed@mail.ru>
2015-09-05 22:20:21 +0700

    add bootstrap

--- app/1.Introduction/application.js
+++ app/1.Introduction/application.js
@@ -1,3 +1,25 @@
-$(function() {
+$(function () {
 
+    // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+    function AppViewModel() {
+
+        //start watch this variable. technically make it like object
+        this.firstName = ko.observable("Bert");
+        this.lastName = ko.observable("Bertington");
+
+        //watch variable with custom logic
+        this.fullName = ko.computed(function () {
+            return this.firstName() + " " + this.lastName();
+        }, this);
+
+        this.capitalizeLastName = function () {
+            // Read the current value. Interact with like with object
+            var currentVal = this.lastName();
+            // Write back a modified value
+            this.lastName(currentVal.toUpperCase());
+        };
+    }
+
+    // Activates knockout.js
+    ko.applyBindings(new AppViewModel());
 });
